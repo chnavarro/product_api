d2c676c6ccc5f32568adab0e87a6e3909d69a417
cnavarro <chnavarrocalderon@gmail.com>
2022-11-03 18:25:41 -0600

    Spring boot rest api - Fix for Forbidden Exception

--- src/main/java/com/tizo/productapi/configuration/security/JwtFilter.java
+++ src/main/java/com/tizo/productapi/configuration/security/JwtFilter.java
@@ -9,6 +9,7 @@ import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
 import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
@@ -21,7 +22,7 @@ public class JwtFilter extends GenericFilterBean {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain filterChain)
             throws IOException, ServletException {
-        Authentication authentication = JwtUtil.getAuthentication((HttpServletRequest) request);
+        Authentication authentication = JwtUtil.getAuthentication((HttpServletRequest) request, (HttpServletResponse) response);
         SecurityContextHolder.getContext().setAuthentication(authentication);
         filterChain.doFilter(request, response);
     }
--- src/main/java/com/tizo/productapi/configuration/security/JwtUtil.java
+++ src/main/java/com/tizo/productapi/configuration/security/JwtUtil.java
@@ -1,12 +1,19 @@
 package com.tizo.productapi.configuration.security;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.tizo.productapi.exception.JWTForbiddenException;
+import io.jsonwebtoken.ClaimJwtException;
+import io.jsonwebtoken.ExpiredJwtException;
 import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 import java.util.Date;
 
 import static java.util.Collections.emptyList;
@@ -30,20 +37,35 @@ public class JwtUtil {
         res.addHeader("Authorization", "Bearer " + token);
     }
 
-    static Authentication getAuthentication(HttpServletRequest request) {
-        String token = request.getHeader("Authorization");
-        if (token != null) {
-            String user = Jwts.parser()
-                    .setSigningKey(SECRET_KEY)
-                    .parseClaimsJws(token.replace("Bearer", ""))
-                    .getBody()
-                    .getSubject();
+    static Authentication getAuthentication(HttpServletRequest request,HttpServletResponse response) throws IOException {
+        try {
+            String token = request.getHeader("Authorization");
+            if (token != null) {
+                String user = Jwts.parser()
+                        .setSigningKey(SECRET_KEY)
+                        .parseClaimsJws(token.replace("Bearer", ""))
+                        .getBody()
+                        .getSubject();
 
-            return user != null ?
-                    new UsernamePasswordAuthenticationToken(user, null, emptyList()) :
-                    null;
+                return user != null ?
+                        new UsernamePasswordAuthenticationToken(user, null, emptyList()) :
+                        null;
+            }
+            return null;
+        } catch (ExpiredJwtException e) {
+            JWTForbiddenException errorResponse = new JWTForbiddenException(e.getMessage());
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            response.getWriter().write(convertObjectToJson(errorResponse));
+            return null;
         }
-        return null;
+    }
+
+    public static String convertObjectToJson(Object object) throws JsonProcessingException {
+        if (object == null) {
+            return null;
+        }
+        ObjectMapper mapper = new ObjectMapper();
+        return mapper.writeValueAsString(object);
     }
 }
 
